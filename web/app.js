dbfd12bdaa072c20fa8454686fe76dcddcda9125
Ruud Verbeek <ruud@arfie.nl>
2018-02-09 15:09:10 +0100

    Table with usernames and reddit names

--- web/app.js
+++ web/app.js
@@ -1,5 +1,11 @@
 var app = angular.module('draftPacketApp', []);
 
-app.controller('draftPacketController', function($scope) {
-    $scope.title = 'Hello World!';
+app.controller('draftPacketController', function($scope, $http) {
+    $scope.packet = [];
+    $scope.loading = true;
+
+    $http.get('packet.json').then(data => {
+        $scope.packet = data.data;
+        $scope.loading = false;
+    });
 });
